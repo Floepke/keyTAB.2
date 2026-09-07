a905246f60d3bde99d1340899564c105b3504b81
Philip Bergwerf <philipbergwerf@gmail.com>
2026-09-07 22:01:07 +0200

    trim

--- file_model/header.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-# Backwards-compatible aliases (header renamed to info).
-from file_model.info import Info as Header
-from file_model.font import Font as FontSpec
-
-HeaderText = str
